d85a0b0da32658a17890686e86ac9a6b72d21d0f
Kim Palao <KimPalao@users.noreply.github.com>
2019-09-05 21:47:56 +0800

    Updated URL regex to match more recent versions

--- topnotchdev/files_widget/urls.py
+++ topnotchdev/files_widget/urls.py
@@ -7,6 +7,6 @@ from django.conf import settings
 from .views import upload, thumbnail_url
 
 urlpatterns = [
-    url(u'^upload/$', upload, name="files_widget_upload"),
-    url(u'^thumbnail-url/$', thumbnail_url, name="files_widget_get_thumbnail_url"),
+    url('^upload', upload, name="files_widget_upload"),
+    url('^thumbnail-url', thumbnail_url, name="files_widget_get_thumbnail_url"),
 ]
